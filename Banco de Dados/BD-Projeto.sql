02c21e27910341be10a5cf83f1c082409edfd9bd
0BlackSmoke0 <bertucci.henrique@gmail.com>
2020-10-15 15:15:54 -0300

    alterei o script e o modelo

--- Banco de Dados/BD-Projeto.sql
+++ Banco de Dados/BD-Projeto.sql
@@ -5,33 +5,40 @@ create table Usuario(
 	IdUsuario int primary key auto_increment,
 	Nome varchar(60),
 	Email varchar(70),
-	Senha varchar(10)
+	Senha varchar(10),
+    fkCNPJ char(14),
+    foreign key (fkcnpj) references Empresa(CNPJ)
 ) auto_increment=100;
 
 
-create table Cadastro(
+create table Empresa(
 	CNPJ char(14) primary key,
 	nomeEmpresa varchar(50),
-	Estado varchar(30),
-	Cidade varchar(30),
-	Rua varchar(100),
-	Numero int,
-	CEP char (8),
     telComercial varchar (15),
-	fkUsuario int,
-	foreign key (fkUsuario) references DadosAcesso(IdUsuario)
+	numero varchar(10),
+    complemento varchar(100)
 );
-insert into DadosEmpresa values
-(03020853000183, 'BandTest','São Paulo','São Paulo','Haddock Caramelo','77','Paulista','08000160','Starbucks', 100);
-select * from DadosEmpresa, DadosAcesso where idEmpresa= fkEmpresa;
-select * from DadosAcesso;
-select * from DadosEmpresa;
-select * from DadosAcesso;
 
-create table sensor(
+create table dados(
 	idSensor int primary key auto_increment,
-    lumens int
+    lumens int,
+    data_resgistro datetime,
+    local_sensor varchar(40),
+    fkCNPJ char (14),
+    foreign key (fkCNPJ) references Empresa(CNPJ)
 );
+
+create table endereco (
+	CEP char(8) primary key,
+    logradouro varchar(80),
+    cidade varchar(50),
+    estado varchar(50),
+    bairro varchar(60),
+    fkCNPJ char(14),
+    foreign key (fkCNPJ) references Empresa(CNPJ)
+);
+
+
 
 
 
